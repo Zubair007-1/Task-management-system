d015a5d32e2e30097f54a1ba965a20296aabe61a
Zubair <syedmohammedzubairz@gmail.com>
2026-07-22 12:28:42 +0530

    Day 2: Added Task entity and configured Hibernate entity mapping

---
+++ taskmanager/src/main/java/com/zubair/taskmanager/entity/Task.java
@@ -0,0 +1,20 @@
+package com.zubair.taskmanager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private String status;
+
+}
